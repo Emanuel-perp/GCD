3a95ea51c75011884a3119388e73c2a78fd2eabb
Emanuel-perp <emanuelperp13@gmail.com>
2023-10-01 22:06:59 -0600

    Se agrega el proyecto

---
+++ GCD/GCD.srcs/sources_1/imports/Diseño de sistemas digitales/control_unit.vhd
@@ -0,0 +1,93 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity control_unit is
+    port(
+        clock, reset, go : in std_logic;
+        flags : in std_logic_vector(2 downto 0);
+        x_we, y_we, x_sel, y_sel, gcd_we: out std_logic
+    );
+end control_unit;
+
+architecture Behavioral of control_unit is
+    type state_t is (start, input, test, update_x, update_y, done);
+    signal current_state, next_state : state_t;
+begin
+state_register:
+    process(clock, reset)
+    begin
+        if reset = '1' then
+            current_state <= start;
+        elsif rising_edge(clock) then
+            current_state <= next_state;
+        end if;
+    end process state_register;
+
+machine_input:
+    process(current_state, flags, go)
+    begin
+        case current_state is
+            when start =>
+                if go = '1' then
+                    next_state <= input;
+                else
+                    next_state <= start;
+                end if;
+            when input =>
+                next_state <= test;
+            when test =>
+                if flags = "010" then
+                    next_state <= done;
+                elsif flags = "100" then
+                    next_state <= update_y;
+                else
+                    next_state <= update_x;
+                end if;
+            when update_x =>
+                next_state <= test;
+            when update_y =>
+                next_state <= test;
+            when done =>
+                next_state <= done;
+            when others =>
+                null;
+        end case;
+    end process machine_input;
+
+machine_output:
+    process(clock, reset, current_state)
+    begin
+        if reset = '1' then
+            x_sel <= '1';
+            y_sel <= '1';
+            x_we <= '0';
+            y_we <= '0';
+            gcd_we <= '0';
+        elsif rising_edge(clock) then
+            case current_state is
+                when input =>
+                    x_sel <= '1';
+                    y_sel <= '1';
+                    x_we <= '1';
+                    y_we <= '1';
+                when test =>
+                    x_we <= '0';
+                    y_we <= '0';
+                when update_x =>
+                    x_sel <= '0';
+                    x_we <= '1';
+                    y_we <= '0';
+                when update_y =>
+                    y_sel <= '0';
+                    y_we <= '1';
+                    x_we <= '0';
+                when done =>
+                    x_we <= '0';
+                    y_we <= '0';
+                    gcd_we <= '1';
+                when others =>
+                    null;
+            end case;
+        end if;
+    end process machine_output;
+end Behavioral;
---
+++ GCD/GCD.srcs/sources_1/imports/Diseño de sistemas digitales/system.vhd
@@ -0,0 +1,70 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity system is
+    port(
+        x, y : in std_logic_vector(7 downto 0);
+        master_clock, reset, button: in std_logic;
+        anodes : out std_logic_vector(3 downto 0);
+        cathodes : out std_logic_vector(6 downto 0)
+    );
+end system;
+
+architecture Behavioral of system is
+    signal slow_clock : std_logic;
+    signal counter : std_logic_vector(18 downto 0);
+    signal flags : std_logic_vector(2 downto 0);
+    signal gcd: std_logic_vector(7 downto 0);
+    signal x7seg_in : std_logic_vector(15 downto 0);
+    signal x_we, y_we, x_sel, y_sel, gcd_we: std_logic;
+    signal delay0, delay1, delay2, delay3, delay4, delay5, debounce, pulse: std_logic;
+begin
+frequency_divider:
+    process(master_clock, reset)
+    begin
+        if reset = '1' then
+            counter <= (others => '0');
+        elsif rising_edge(master_clock) then
+            counter <= counter + 1;
+        end if;
+    end process frequency_divider;
+    slow_clock <= counter(18);
+button_debounce:
+    process(slow_clock, reset)
+    begin
+        if reset = '1' then
+            delay0 <= '0';
+            delay1 <= '0';
+            delay2 <= '0';
+        elsif rising_edge(slow_clock) then
+            delay0 <= button;
+            delay1 <= delay0;
+            delay2 <= delay1;
+        end if;
+    end process button_debounce;
+    debounce <= delay0 and delay1 and delay2;
+button_pulse:
+    process(slow_clock, reset)
+    begin
+        if reset = '1' then
+            delay3 <= '0';
+            delay4 <= '0';
+            delay5 <= '0';
+        elsif rising_edge(slow_clock) then
+            delay3 <= debounce;
+            delay4 <= delay3;
+            delay5 <= delay4;
+        end if;
+    end process button_pulse;
+    pulse <= delay3 and delay4 and (not delay5);
+U0:
+    entity work.data_path port map(clock => master_clock, reset => reset, x_in => x, y_in => y, x_sel => x_sel, y_sel => y_sel, x_we => x_we, y_we => y_we, gcd_we => gcd_we, flags => flags, gcd => gcd);
+U1:
+    entity work.control_unit port map(clock => master_clock, reset => reset, go => pulse, flags => flags, x_we => x_we, y_we => y_we, x_sel => x_sel, y_sel => y_sel, gcd_we => gcd_we);
+U3:
+    entity work.x7seg port map(clock => slow_clock, reset => reset, x => x7seg_in, anodes => anodes, cathodes => cathodes);
+    x7seg_in(15 downto 8) <= (others => '0');
+    x7seg_in(7 downto 0) <= gcd;
+end Behavioral;
+
